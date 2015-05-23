5b97a4f6527f994e55267d7a05f53f456f81d164
William Talmadge <willtalmadge@gmail.com>
2015-05-23 11:00:49 -0600

    Added packages and dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+
 setup(name='cryostation',
       version='0.1',
       description='A library for communicating with a Montana Instruments Cryostation',
@@ -6,5 +7,12 @@ setup(name='cryostation',
       author='William Talmadge',
       author_email='willtalmadge@gmail.com',
       license='MIT',
-      packages=['cryostation'],
+      packages=[
+          'cryostation',
+          'cryostation.rx'
+      ],
+      install_requires=[
+            'rx',
+            'numpy'
+      ],
       zip_safe=False)
